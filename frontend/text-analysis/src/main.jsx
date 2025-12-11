55d087516c9769ad08012b528253a9bc6e1eca40
unknown <ai.devhub2@gmail.com>
2025-12-11 22:53:41 +0530

    Setup the react structure

--- frontend/text-analysis/src/main.jsx
+++ frontend/text-analysis/src/main.jsx
@@ -2,11 +2,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import App from "./App.jsx";
+import { Login } from "./pages/Login.jsx";
+import { Signin } from "./pages/Signin.jsx";
+import { Home } from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
